b03a00683f7b1ad3298a51bdc52399344ec9bfa0
Mihir Verma <mihir.v2002@gmail.com>
2024-06-22 11:36:18 +0530

    Add D:\DSA\Demo.java (created 2024-06-22 11:36:18)

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println("Hey "+x);
+        scanner.close();
+    }
+}
+
+// TO Run a Code use Ctrl + Shift + b
