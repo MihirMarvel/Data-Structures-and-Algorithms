319c7b274e8881e1776509a6f035f7f11ecfef5a
Mihir Verma <mihir.v2002@gmail.com>
2024-08-03 23:44:52 +0530

    Add D:\DSA\basic\patterns\pattern5.java (created 2024-08-03 23:44:52)

---
+++ basic/patterns/pattern5.java
@@ -0,0 +1,21 @@
+package basic.patterns;
+
+public class pattern5 {
+    public static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j<n-i){
+                    System.out.print("*"+" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Pattern\n");
+
+        pattern(5);
+    }
+}
