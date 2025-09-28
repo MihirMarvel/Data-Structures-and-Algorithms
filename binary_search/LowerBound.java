51121aa9639584c884556e6e8495fed391b00f76
Mihir Verma <mihir.v2002@gmail.com>
2025-09-28 13:13:43 +0530

    Implemented upper and lower bound using binary search

---
+++ binary_search/LowerBound.java
@@ -0,0 +1,42 @@
+package binary_search;
+
+import java.util.Scanner;
+
+public class LowerBound {
+
+    public static int findLowerBound(int[] nums, int target) {
+
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int min = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
+                min = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int num = findLowerBound(nums, target);
+
+        System.out.println("Lower bound of " + target + " at index :- " + num);
+
+        sc.close();
+
+    }
+
+}
